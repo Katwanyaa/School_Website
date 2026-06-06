c57d99737b44ca2ea3c6d5bf000542cda63c6931
Emmanuel Makau <emmanuelmakau90@gmail.com>
2026-06-06 20:16:11 +0300

    added the change to the main brnach

--- app/pages/admissions/page.jsx
+++ app/pages/admissions/page.jsx
@@ -1,7 +1,7 @@
 'use client';
 import { useState, useEffect } from 'react';
 import { toast, Toaster } from 'sonner';
-import { cleanGeneratedFileName } from '../../libs/displayNames';
+import { cleanGeneratedFileName } from '../../../libs/displayNames';
 import { 
   FiUser, 
   FiCalendar, 
